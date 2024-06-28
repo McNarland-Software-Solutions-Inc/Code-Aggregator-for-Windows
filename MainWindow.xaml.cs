fb30df9b0754eae1ccf3d845957e2c30d1fc027e
Robert McNarland <robert.mcnarland@example.com>
2024-06-28 10:21:17 -0700

    Port Windows Forms application to Avalonia for cross-platform support

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Code_Aggregator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
